532d56fb6171c08d8cf9a7b92a63ed115ddd11cb
Marianna Ojanen <marianna.ojanen@kotiposti.net>
2022-03-29 19:18:48 +0300

    4&5 Kahden eri tuotteen jälkeen korissa 2 tavaraa ja hinta oikein

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,14 +5,22 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote("Maito", 3)
+        self.mehu = Tuote("Mehu", 4)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara_ja_hinta_oikein(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
 
         self.assertEqual(self.kori.hinta(), 3)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
+    
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tuotetta_ja_hinta_oikein(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.mehu)
+
+        self.assertEqual(self.kori.hinta(), 7)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
